a47043dc4edaa561a622e94b0f10fd0b17d547d3
soori <tharukaravisra@gmail.com>
2023-11-30 22:35:34 +0530

    Initial Push

---
+++ src/main/java/soori/medifindpro/MediFindProApplication.java
@@ -0,0 +1,13 @@
+package soori.medifindpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFindProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediFindProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soori/medifindpro/MediFindProApplicationTests.java
@@ -0,0 +1,13 @@
+package soori.medifindpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFindProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
